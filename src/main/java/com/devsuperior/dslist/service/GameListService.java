262b180a746cd34aac95cb9f1d057f4c8cda4767
Guilherme Silva Pereira <silvaarthur.pereira123@gmail.com>
2023-05-12 00:59:11 -0300

    Add Projection

--- src/main/java/com/devsuperior/dslist/service/GameListService.java
+++ src/main/java/com/devsuperior/dslist/service/GameListService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.ropository.GameListRepository;
 
@@ -15,10 +17,18 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameService gameService;
+	
 	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
 	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(Long lisId) {
+		List<GameMinDTO> result = gameService.fingByList(lisId);
+		return result;
+	}
 }
--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -8,9 +8,8 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projection.GameMinProjection;
 import com.devsuperior.dslist.ropository.GameRepository;
-
-import jakarta.transaction.Transactional;
 
 @Service // Registra a classe como componente do Sistema.
 public class GameService {
@@ -27,6 +26,12 @@ public class GameService {
 	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	public List<GameMinDTO> fingByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
