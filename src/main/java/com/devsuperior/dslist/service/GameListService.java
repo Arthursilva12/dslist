96c36bc35e7c2d34367546d14e156df10f834fb3
Guilherme Silva Pereira <silvaarthur.pereira123@gmail.com>
2023-05-24 18:08:55 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/service/GameListService.java
+++ src/main/java/com/devsuperior/dslist/service/GameListService.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.devsuperior.dslist.dto.GameListDTO;
-import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projection.GameMinProjection;
 import com.devsuperior.dslist.ropository.GameListRepository;
+import com.devsuperior.dslist.ropository.GameRepository;
 
 @Service // Registra a classe como componente do Sistema.
 public class GameListService {
@@ -18,7 +18,7 @@ public class GameListService {
 	private GameListRepository gameListRepository;
 	
 	@Autowired
-	private GameService gameService;
+	private GameRepository gameRepository;
 	
 	@org.springframework.transaction.annotation.Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
@@ -26,9 +26,18 @@ public class GameListService {
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
 	
-	@GetMapping(value = "/{listId}/games")
-	public List<GameMinDTO> findByList(Long lisId) {
-		List<GameMinDTO> result = gameService.fingByList(lisId);
-		return result;
+	public void move(Long listId, int sourceIndex, int destinaionIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinaionIndex ,obj);
+		
+		int min = sourceIndex < destinaionIndex ? sourceIndex : destinaionIndex;
+		int max = sourceIndex < destinaionIndex ? destinaionIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
+	
+	
 }
